5c054af8feec39ba94186d0137027365fcb219d6
Stefan Nieuwenhuis <stefannhs@gmail.com>
2026-02-09 11:20:07 +0100

    add logging default config

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
 def main():
     print("Hello from postnl-data-engineering-assessment!")
 
