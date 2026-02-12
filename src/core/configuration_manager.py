7488c629f2bb7ae2715f3a0254793b4a576eb0da
Stefan Nieuwenhuis <stefannhs@gmail.com>
2026-02-12 14:58:32 +0100

    add layer bucket property to get_quarantine_path for more flexibility

--- src/core/configuration_manager.py
+++ src/core/configuration_manager.py
@@ -148,12 +148,13 @@ class ConfigurationManager:
         """
         return self.get("merge_keys", dataset_name)
 
-    def get_quarantine_path(self, dataset_name: str) -> str:
+    def get_quarantine_path(self, dataset_name: str, layer: str = "bronze") -> str:
         """
         Get quarantine path for corrupt/validation-failed records.
 
         :param dataset_name: Dataset name
+        :param layer: Layer bucket
         :return: Full path for quarantine Delta table
         """
-        bucket = self.get_bucket("bronze")
+        bucket = self.get_bucket(layer)
         return f"{bucket}/_quarantine/{dataset_name}"
